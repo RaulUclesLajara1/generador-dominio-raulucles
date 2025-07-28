ad103991a0005e39360e4655321786ae86b9912c
Raúl Uclés Lajara <rul2@alu.ua.es>
2025-07-28 12:40:22 +0200

    Generador de dominios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['el','mi','un','tu'];
+
+let adj = ['gran', 'magico','tenebroso','duro'];
+
+let noun = ['gato', 'rinoceronte','armario','cojin'];
+
+let extensiones = ['.com', '.es', '.net','.org'] 
+let nombre_dominio;
+
+for(let i=0;i<pronoun.length;i++){
+  let pronombre=pronoun[i];
+
+  for(let j=0; j<adj.length;j++){
+    let adjetivo=adj[j];
+      
+    for(let k=0;k<noun.length;k++){
+      let sustantivo=noun[k]
+      let extension=extensiones[Math.floor(Math.random()*4)]
+      console.log(pronombre+adjetivo+sustantivo+extension)
+      }
+    }
+  }
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
